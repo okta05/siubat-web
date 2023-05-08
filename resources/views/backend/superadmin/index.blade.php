c8db8aabe59a87f63ecaba96d1af6b47fb037de0
Raufil Pratama <74433586+rinoragami45@users.noreply.github.com>
2023-05-08 14:44:22 +0700

    modified:   resources/views/backend/superadmin/index.blade.php modified:   resources/views/backend/superadmin/superadmin_master.blade.php modified:   resources/views/backend/superadmin/user/view_user.blade.php

--- resources/views/backend/superadmin/index.blade.php
+++ resources/views/backend/superadmin/index.blade.php
@@ -15,22 +15,20 @@
           <!-- Card Pengajuan -->
           @if(auth()->user()->usertype=="superadmin")
           <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card1 border-left-primary shadow h-100 py-2">
+              <div class="card1 border-left-success shadow h-100 py-2">
                   <div class="card-body">
                       <div class="row no-gutters align-items-center">
                           <div class="col mr-2">
-                              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                  User
-                              </div>
-                              <div class="h5 mb-0 font-weight-bold text-gray-800">{{auth()->user()->count()}}</div>
+                              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                  Event</div>
+                              <div class="h2 mb-0 font-weight-bold text-gray-800">{{auth()->user()->count()}}</div>
                           </div>
                       </div>
-                      <div class="card-footer">
-                          <a href="{{route('view_user')}}" class="card-link">
-                              Lihat Detail
-                              <i class="fas fa-arrow-circle-right"></i>
-                          </a>
-                      </div>
+                      <hr />
+                        <a href="{{route('view_user')}}" class="btn mt-3 float-right" style="background-color:#83D350; color:#513C2F; border-top: 1px solid #513C2F;">
+                            Lihat Detail
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </a>
                   </div>
               </div>
           </div>
--- resources/views/backend/superadmin/superadmin_master.blade.php
+++ resources/views/backend/superadmin/superadmin_master.blade.php
@@ -64,7 +64,7 @@
                     <div class="modal-body">apakah anda yakin ingin keluar?</div>
                     <div class="modal-footer">
                         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="{{route('admin.logout')}}">Logout</a>
+                        <a class="btn btn-danger" href="{{route('admin.logout')}}">Logout</a>
                     </div>
                 </div>
             </div>
