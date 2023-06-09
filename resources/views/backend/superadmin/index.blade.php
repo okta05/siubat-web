ff914407d38d118c120335408246d8e0bf41b063
Kezia Megawati <keziamega13@gmail.com>
2023-06-09 09:45:31 +0700

    modif tulisan

--- resources/views/backend/superadmin/index.blade.php
+++ resources/views/backend/superadmin/index.blade.php
@@ -41,7 +41,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Event</div>
+                                Acara</div>
                             <div class="h2 mb-0 font-weight-bold text-gray-800">{{ $eventCount }}</div>
                         </div>
                     </div>
