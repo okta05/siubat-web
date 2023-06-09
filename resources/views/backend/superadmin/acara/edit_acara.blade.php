057d98866c7107a4c7fe8dcad81ce214e2e1183c
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2023-06-09 09:49:02 +0700

    fix view acara

--- resources/views/backend/superadmin/acara/edit_acara.blade.php
+++ resources/views/backend/superadmin/acara/edit_acara.blade.php
@@ -5,7 +5,7 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard | Ubah Data Event</h1>
+        <h1 class="h3 mb-0 text-gray-800">Dashboard | Ubah Data Acarat</h1>
     </div>
 
     <!-- Content Row -->
@@ -17,7 +17,7 @@
             <div class="card shadow mb-4">
                 <!-- Card Header - Edit -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-dark">Ubah Data Event</h6>
+                    <h6 class="m-0 font-weight-bold text-dark">Ubah Data Acara</h6>
                 </div>
 
                 <!-- Card Content - Edit -->
